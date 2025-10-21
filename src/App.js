0261d9128f1d51f6fa388c6cebe53825688892ff
1Gab13riel1 <gabriel323547@gmail.com>
2025-10-20 19:13:39 -0500

    image added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ function App(){
         <div className='App'>
             <h1>App className</h1>
             <header className="App-header">
+                <img
+                    src="http://pngimg.com/image/12489"
+                    alt="Ops no img loading"
+                    style={{ 
+	                    width: "100%", 
+	                    maxWidth: "600px",
+	                    height: "auto"
+                    }} />
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
                     Edit <code>src/App.js</code> and save to reload.
